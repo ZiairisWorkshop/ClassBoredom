57bde72e092ab9eb43195aff17bfabaf9b31e393
ZiairisWorkshop <kbenedum2677@flpoly.org>
2015-06-04 17:37:31 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
+# Create a basic job.
 
+#These variables are imported from main game:
+Charisma = 2
+Stamina = 2
+Energy = 15
+
+Coins = 18
+
+TownRep = 34
+Time = 892
+
+# These variables are new.
+PayRate = 5
+
+# Charima and Town Rep calculate PayRate Change
+PayRate = Payrate*(TownRep/2)
+
+if Charisma >5:
+    Payrate = Payrate * (Charima/5)
